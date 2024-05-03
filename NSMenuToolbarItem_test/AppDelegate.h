ffc644600fd73dcede65bd5c6462605ef05a120b
Gregory Casamento <greg.casamento@gmail.com>
2024-05-03 14:18:39 -0400

    Initial Commit

---
+++ NSMenuToolbarItem_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSMenuToolbarItem_test
+//
+//  Created by Gregory John Casamento on 5/3/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
